b8311e9a7884e17da0b107025fcecdfddbf22cef
Eric Wieser <wieser.eric@gmail.com>
2017-05-30 18:08:16 +0100

    Add plots showing failure

--- scripts/compare_learning.m
+++ scripts/compare_learning.m
@@ -3,9 +3,9 @@ dirs = struct();
 % dirs.sim_90 = 'P:\scenarios\unicycle\logs\nlds_7@aa982a8+ uniform_cost';
 % dirs.sim_17 = 'P:\scenarios\unicycle\logs\nlds_8@9237313+ 17degree';
 % dirs.sim_45 = 'P:\scenarios\unicycle\logs\nlds_15@c99a440+';
-dirs.sim = 'P:\scenarios\unicycle\logs\nlds_10@1e09a71 17 degree cost';
-dirs.hardware =	'C:\Users\wiese\Documents\Courses\Project\mlg-ctrl\scenarios\unicycle\logs\nlds_55@dc9584f+';
-
+% dirs.sim = 'P:\scenarios\unicycle\logs\nlds_10@1e09a71 17 degree cost';
+dirs.hardware1 =	'C:\Users\wiese\Documents\Courses\Project\mlg-ctrl\scenarios\unicycle\logs\nlds_55@dc9584f+ hardware';
+dirs.hardware2 = 'C:\Users\wiese\Documents\Courses\Project\mlg-ctrl\scenarios\unicycle\logs\nlds_77@9a78f4e+';
 dirs.sim_nocost_17 = 'P:\scenarios\unicycle\logs\limit_nocost_17_1@917ba0a+';
 dirs.sim_nocost_45 = 'P:\scenarios\unicycle\logs\limit_nocost_45_1@917ba0a+';
 dirs.sim_nocost_90 = 'P:\scenarios\unicycle\logs\limit_nocost_90_1@917ba0a+';
@@ -15,7 +15,8 @@ dirs.sim_cost_45 = 'P:\scenarios\unicycle\logs\limit_cost_45_1@917ba0a+';
 dirs.sim_cost_90 = 'P:\scenarios\unicycle\logs\limit_cost_90_1@917ba0a+';
 
 N = struct();
-N.hardware = 23;  % only 23 hardware runs
+N.hardware1 = 23;  % only 23 hardware runs
+N.hardware2 = 38;
 N.sim = 40;
 N.sim_nocost_17 = 40;
 N.sim_nocost_45 = 40;
@@ -41,18 +42,22 @@ end
 %%
 lengths = struct();
 for f = fields(dirs)', f = f{1};
-	lengths.(f) = arrayfun(@(t) t.dt * size(t.observed, 2), data.(f).trajectories);
+	lengths.(f) = arrayfun(@(t) t.dt * size(t.action, 2), data.(f).trajectories);
 end
+lengths.hardware1 = lengths.hardware1 - data.hardware1.trajectories(1).dt;
 %%
 names = struct();
-names.hardware = 'experimental';
-names.sim = 'simulated';
+names.hardware1 = 'experiment 1';
+names.hardware2 = 'experiment 2';
+names.sim_cost_17 = 'simulated';
 
 
 plot_it_graph(lengths, names);
+grid on;
 savetikz('learning-progress-iteration', '5cm');
 
 plot_time_graph(lengths, names);
+grid on;
 savetikz('learning-progress-time', '5cm');
 %%
 names = struct();
